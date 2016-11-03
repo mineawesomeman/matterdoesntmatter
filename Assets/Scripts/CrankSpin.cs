a2a8564c2e1ae2236b26471f5e69630aa5547a52
mineawesomeman <mineawesomeman@gmail.com>
2016-11-03 15:18:31 -0400

    Useless Comments
    Also added some confusions..

--- Assets/Scripts/CrankSpin.cs
+++ Assets/Scripts/CrankSpin.cs
@@ -1,46 +1,19 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CrankSpin : MonoBehaviour {
 
-   public GameObject crank;
-   /*public AudioSource sound;
-   public AudioClip clip, clip2, clip3;
-   public int random;*/
-
+   public GameObject crank; //When is this used?!?!?!
+	
    void Update ()
     {
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
-        if (scroll > 0f || scroll < 0f || Input.GetKey(KeyCode.Q))
+        if (scroll > 0f || scroll < 0f || Input.GetKey(KeyCode.Q)) //Why is scroll used here and at Shooting.cs ?
         {
 
-            this.gameObject.transform.Rotate(new Vector3(0, 0, -10));
-
-            /*random = Random.Range(1, 4);
-
-            if (random == 1)
-            {
-
-                sound.clip = clip;
-
-            }
-            if (random == 2)
-            {
-
-                sound.clip = clip2;
-
-            }
-            if (random == 3)
-            {
-
-                sound.clip = clip3;
-
-            }
-
-            sound.clip = clip;
-            sound.Play();*/
+            this.gameObject.transform.Rotate(new Vector3(0, 0, -10)); //Rotating? What For?
 
         }//end cranking to add energy
 
